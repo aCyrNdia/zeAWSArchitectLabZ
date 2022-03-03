d5a983c8de2b2fdb3fb86d27eca2763cf895cdce
aCyrNdia <87100536+aCyrNdia@users.noreply.github.com>
2022-03-03 12:00:30 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
 variable "access-key-id" {
   type = string
-  default = "AKIAUMFA7UMTTXENG25K"
+  default = "AKIA3CMB4IBY23TBCQQS"
 }
 
 variable "secret-key" {
   type    = string
-  default = "bu8SI6ldD+die1szHBK172S1yOBFF0eGikMZ59Z2"
+  default = "zbjspXNNDSzu2H4/llxi5u6HjDQtclfUW5vHNnK9"
+}
+
+variable "ami-id" {
+  type    = string
+  default = "ami-0c293f3f676ec4f90"
 }
