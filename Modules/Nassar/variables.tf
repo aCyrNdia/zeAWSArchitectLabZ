a07f3762eaea502c725343556010cd2c53706bf7
aCyrNdia <87100536+aCyrNdia@users.noreply.github.com>
2022-03-03 11:03:42 +0000

    Delete variables.tf

--- Modules/Nassar/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access-key-id" {
-  type = string
-  default = "AKIA3CMB4IBY23TBCQQS"
-}
-
-variable "secret-key" {
-  type    = string
-  default = "zbjspXNNDSzu2H4/llxi5u6HjDQtclfUW5vHNnK9"
-}
